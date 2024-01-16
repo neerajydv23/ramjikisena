8c8cfd77fdb0814b2a6a975ca4d8921ee3ff4b22
Neeraj <yneeraj082r@gmail.com>
2024-01-16 21:49:40 +0530

    second commit

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-// mongoose.connect("mongodb://127.0.0.1:27017/ramnaambank");
-const Connection = async () => {
-  const URL = 'mongodb+srv://yneeraj082r:jT5N0eg3sTUj0XFy@ramnaambank.vyriomp.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect("mongodb://127.0.0.1:27017/ramnaambank");
+// const Connection = async () => {
+//   const URL = 'mongodb+srv://yneeraj082r:jT5N0eg3sTUj0XFy@ramnaambank.vyriomp.mongodb.net/?retryWrites=true&w=majority';
   
-  try {
-    await mongoose.connect(URL);
-    console.log('Database connected successfully');
-  } catch (error) {
-    console.error('Error connecting to the database', error);
-  }
-};
+//   try {
+//     await mongoose.connect(URL);
+//     console.log('Database connected successfully');
+//   } catch (error) {
+//     console.error('Error connecting to the database', error);
+//   }
+// };
 
-Connection();
+// Connection();
 
 
 const userSchema = mongoose.Schema({
@@ -23,7 +23,22 @@ const userSchema = mongoose.Schema({
   password:String,
   profileImage:String,
   contact:Number,
-// ramnaam count and ramnam schema
+  prevCount:{
+    type:Number,
+    default:"0"
+  },
+  currCount:{
+    type:Number,
+    default:"0"
+  },
+  totalCount:{
+    type:Number,
+    default:"0"
+  },
+  rank:{
+    type:Number,
+    default:"0"
+  }
 });
 
 userSchema.plugin(plm);
