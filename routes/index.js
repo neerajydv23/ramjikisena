6da160b2a3da64c1327e46f594e2bbfef962baa7
Neeraj <yneeraj082r@gmail.com>
2024-01-16 18:57:58 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const userModel = require('./users')
+
+passport.use(new localStrategy(userModel.authenticate()))
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {error: req.flash('error')});
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', {error: req.flash('error')});
+});
+
+router.get('/profile', isLoggedIn,function(req, res, next) {
+  res.render('profile',{user:req.user});
+});
+
+router.post('/register', function(req, res, next) {
+
+  if (!req.body.username || !req.body.fullname || !req.body.email || !req.body.password) {
+    req.flash('error', 'All fields are required');
+    return res.redirect('/register');
+  }
+
+  const data = new userModel({
+    username:req.body.username,
+    email:req.body.email,
+    contact:req.body.contact,
+    name:req.body.fullname
+  })
+  userModel.register(data,req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile");
+    })
+  })
+  .catch(function (err) {
+    // Handle registration failure (e.g., username/email already taken)
+    req.flash('error', 'Registration failed. Please choose a different username or email.');
+    res.redirect('/');
+  });
+});
+
+router.post('/login', passport.authenticate("local",{
+  failureRedirect:"/",
+  successRedirect:"/profile",
+  failureFlash:true
+}),function(req,res,next){
+});
+
+router.get('/logout',function(req,res,next){
+  req.logout(function(err){
+    if(err){return next(err);}
+    res.redirect('/');
+  });
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+// mongoose.connect("mongodb://127.0.0.1:27017/ramnaambank");
+const Connection = async () => {
+  const URL = 'mongodb+srv://yneeraj082r:jT5N0eg3sTUj0XFy@ramnaambank.vyriomp.mongodb.net/?retryWrites=true&w=majority';
+  
+  try {
+    await mongoose.connect(URL);
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+  }
+};
+
+Connection();
+
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name:String,
+  email:String,
+  password:String,
+  profileImage:String,
+  contact:Number,
+// ramnaam count and ramnam schema
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user",userSchema);
